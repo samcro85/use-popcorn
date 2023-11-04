e6dacc18e7a4e8085c5750d317a8567eb3641d4e
samcro85 <snowsurf85@gmail.com>
2023-11-04 08:55:32 +0100

    creato component Search, Logo e NumResults su Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,30 @@
 import { useState } from "react";
 
-
 export default function Navbar() {
-    const [query, setQuery] = useState("");
-
   return (
     <nav className="nav-bar">
       <div className="logo">
         <span role="img">🍿</span>
         <h1>usePopcorn</h1>
       </div>
-      <input
-        className="search"
-        type="text"
-        placeholder="Search movies..."
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
-      />
+      <Search />
       <p className="num-results">
         Found <strong>X</strong> results
       </p>
     </nav>
+  );
+}
+
+function Search() {
+  const [query, setQuery] = useState("");
+
+  return (
+    <input
+      className="search"
+      type="text"
+      placeholder="Search movies..."
+      value={query}
+      onChange={(e) => setQuery(e.target.value)}
+    />
   );
 }
