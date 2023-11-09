8090580f6a4cfb66c944ccad5f100ef5d626b209
samcro85 <snowsurf85@gmail.com>
2023-11-09 08:10:56 +0100

    implementato typeprops check'

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './components/StarRating';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import './index.css';
+import App from './App';
+// import StarRating from "./components/StarRating";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <StarRating maxRating={5}/>
-    {/* <App /> */}
+    {/* <StarRating maxRating={5} message={['Terrible', 'Bad', 'Okay', 'Good', 'Amazing']}/> */}
+    <App />
+    {/* <StarRating color="purple" size={24} className='test' defaultRating={3}/> */}
   </React.StrictMode>
 );
-
